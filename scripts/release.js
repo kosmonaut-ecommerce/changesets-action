6fb0e899701d7c90c531725781f7a7bfb7348ea8
marcelfrenz <mf@kosmonaut.io>
2024-04-25 08:52:41 +0200

    chore: added --no-verify to git commit (skip pre-commit linting)

--- scripts/release.js
+++ scripts/release.js
@@ -27,7 +27,7 @@ process.chdir(path.join(__dirname, ".."));
 
   await exec("git", ["checkout", "--detach"]);
   await exec("git", ["add", "--force", "dist"]);
-  await exec("git", ["commit", "-m", tag]);
+  await exec("git", ["commit", "-m", tag, "--no-verify"]);
 
   await exec("changeset", ["tag"]);
 
